50952710a42a31bb7993f8737604f29e7a594627
Nellyojay <nellyjohnson696@gmail.com>
2026-09-19 15:39:43 +0300

    Fix homepage URL and base path in configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/mysticky/',
+  base: '/mystickynote/',
   plugins: [react(), tailwindcss()],
 })
